c55a2fe63619e80504f74c0dcc8e01b7b4dcb517
Anuar234 <104306302+Anuar234@users.noreply.github.com>
2025-04-12 16:37:01 +0500

    vue addded

--- todoapp/src/App.vue
+++ todoapp/src/App.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="app_container>
-  <router-view />
-  <Navbar />
+  <div class="app_container">
+
+    <router-view />
+
+    <Navbar></Navbar>
+
+
   </div>
 </template>
 
-
 <script>
-import Navbar from 
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App'
+  components {
+    Navbar
+  }
+}
 </script>
+
+
+<style scoped>
+.app_container {
+  display: flex;
+  flex-direction: column;
+  height: 98vh;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  background: linear-gradient(to bottom right, #f0f0f0, #ffffff);
+}
+</style>
---
+++ todoapp/src/views/TaskView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="tasks_container">
+        <div class="tasks-header">
+            <input 
+            v-model="newTask"
+            type="text"
+            placeholder="Enter a task"
+            class="task-input"
+            @keyup.enter="createTask"
+            />
+            <button @click="createTask" class="add-button">+</button>
+        </div>
+        
+        <div class="tasks-list">
+            <div
+            v-for="task in tasks"
+            :keys="task.id"
+            class="task-item"
+            >
+                <div class="task-next">
+                    {{ task.title }}
+                </div>
+                <button
+                class="complete-button"
+                @click="completeTask(task.id)">Done
+            </button>
+        </div>
+        </div>
+    </div>
+</template>
